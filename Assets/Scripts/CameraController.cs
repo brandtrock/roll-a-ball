48dc76f890f1fee4a04a06d50e0834f2b9f0efb6
Brandt Jorgensen <bmjorgensen@outlook.com>
2016-07-07 19:40:18 -0500

    Unity Assets
    Only the assets folder for the roll a ball game.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player; // reference to the player object
+
+    private Vector3 offset; // hold the offset value for the camera
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame - after all items have been processed in normal "Update"
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Allows it to show up in the editor
+	public float speed;
+    public Text countText;
+    public Text winText;
+
+	private Rigidbody rb;
+    private int count; // track collectible game objects
+
+	// Runs on the first frame of the game
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+	}
+
+	// Physics Code Goes Here
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+    // SphereCollider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    // Display text for the number of collected items
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+}
